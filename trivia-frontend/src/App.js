d69cb567c8dabe5a3ef071609e99544f04571538
Mayank Madan <my905874@dal.ca>
2023-07-11 17:49:54 -0300

    added routes

--- trivia-frontend/src/App.js
+++ trivia-frontend/src/App.js
@@ -1,15 +1,19 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HomeScreen from './screens/HomeScreen';
+import GameLobby from './components/GameLobby';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import GameDetails from './components/GameDetails';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Navigate replace to="/home"/>} />
+          <Route exact path="/" element={<Navigate replace to="/home" />} />
           <Route exact path="/home" Component={HomeScreen} />
+          <Route exact path="/game-lobby" Component={GameLobby} />
+          <Route exact path="/game-details/:id" Component={GameDetails} />
         </Routes>
       </Router>
     </>
